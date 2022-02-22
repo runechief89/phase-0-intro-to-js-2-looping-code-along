62384d6615ca3908d2fa320e38d1032d7e5da805
Min <runechief89@gmail.com>
2022-02-21 23:08:30 -0500

    commit tree 5c75df42e8eb1ab3dbc7abadfb606cd47092b320

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+const gifters = ["Guadalupe", "Ollie","Aki"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+function writeCards([], occasion){
+    let giftMessages = [];
+
+    for (let i = 0; i < gifters.length; i++) {
+        giftMessages.push(`Thank you, ${gifters[i]}, for the wonderful ${occasion} gift!`);
+    };
+    return giftMessages;
+}
+
+wrapGifts(gifts);
+
+writeCards(gifters, surprise);
+    
